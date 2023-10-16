7ac00da7747d6f4df60a9bfef7823cd5545855a9
Reuben Germain <reuben.germain@soprasteria.com>
2023-10-16 18:26:34 +0100

    destructered props

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src='https://robohash.org/Imhotep?200x200' alt='robots'/>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
 }
 
-export default Card
+export default Card;
