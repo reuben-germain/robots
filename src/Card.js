aef3a857e777c0330238ddad198bcf635fcede00
Reuben Germain <reuben.germain@soprasteria.com>
2023-10-16 16:41:59 +0100

    created a card component & added a name, email, and used Robohash.org to generate an image

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src='https://robohash.org/Imhotep' alt='robots'/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card
